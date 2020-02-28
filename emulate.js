196107bdc24a92c679dc38a2b192dd1c118341c6
Hans Tool <hans@Hanss-MacBook-Pro.local>
2020-02-28 20:04:34 +0100

    Skip length byte

--- emulate.js
+++ emulate.js
@@ -163,7 +163,11 @@ function mainLoop () {
         if (msg.pgn.pgn == 130850) { // Simnet Event, requires reply
           // Using 130850 and turning it into 130851
           debug ('Reply AP command: %j %j', msg.pgn, msg.data);
-          reply130851 = reply130851.concat(buf2hex(msg.data).slice(1)); // Add multipart Data
+          if (reply130851.length == 0) {
+            reply130851 = reply130851.concat(buf2hex(msg.data).slice(2)); // Skip multipart byte and length byte
+          } else {
+            reply130851 = reply130851.concat(buf2hex(msg.data).slice(1)); // Skip only multipart byte
+          }
           debug('reply130851 %j', reply130851);
           if (reply130851.length > 8) { // We have 2 parts now
               msg = "%s,7,130851,%s,255,12," + reply130851.join(',');
