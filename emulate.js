63dac83d35570d4f33c91dde91a7ff816ec458df
Hans Tool <hans@Hanss-MacBook-Pro.local>
2020-05-01 13:29:24 +0200

    bla

--- emulate.js
+++ emulate.js
@@ -530,8 +530,7 @@ function mainLoop () {
               } else if (PGN130850.match(/^0c,41,9f,01,ff,ff,..,06,00,auto/)) { // Route/navigation
                 pgn126720 = "%s,3,126720,%s,%s,16,3b,9f,f0,81,86,21,03,fc,3c,42,00,00,00,00,ff,ff,ff,ff,ff";
                 debug('Setting Seatalk1 pilot mode Route (navigation)');
-          //  } else if (PGN130850.match(/^0c,41,9f,01,ff,ff,0a,09,00,ff,ff,ff/)) { // Auto
-              } else if (PGN130850.match(/^0c,41,9f,0c,ff,ff,..,1c,00,ff,ff,ff/)) { // Auto
+              } else if (PGN130850.match(/^0c,41,9f,01,ff,ff,..,09,00,ff,ff,ff/)) { // Auto
                 pgn126720 = "%s,3,126720,%s,%s,16,3b,9f,f0,81,86,21,01,fe,00,00,00,00,00,00,ff,ff,ff,ff,ff";
                 debug('Setting Seatalk1 pilot mode Auto');
 
